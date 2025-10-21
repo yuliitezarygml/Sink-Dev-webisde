33bf7f7bf41f4e50d13cf632f236e7596739def6
yuliitezarygml <yuliitezarywork@gmail.com>
2025-10-21 18:41:01 +0300

    +

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import React from "react";
 import { ProjectInfo } from "./pages/ProjectInfo";
 import { ChatButton } from "./pages/ChatButton";
-import { PhotoGallery } from "./pages/PhotoGallery";
 import "./App.css";
 
 const App: React.FC = () => (
     <div className="main-bg">
         <ProjectInfo />
-        <PhotoGallery />
         <ChatButton />
     </div>
 );
